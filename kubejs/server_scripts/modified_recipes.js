11e5f76e0d617f480e5ed36abf121d98f0c37409
ModifiedMC <modifiedminecrafters@gmail.com>
2025-08-07 22:08:48 -0400

    initial commit - cloned plus repo, imported essentials files

--- kubejs/server_scripts/modified_recipes.js
+++ kubejs/server_scripts/modified_recipes.js
@@ -1,25 +1,116 @@
+//
+// Copyright (c) 2025 ImpavidEh - https://www.curseforge.com/members/impavideh/projects 
+// (@Jaker.zzz on Discord) - https://discord.gg/6R6Ch2mNgZ
+// 
+// 
+// This file is for use only in "MODIFIED COBBLEMON" projects.
+// https://ModifiedMC.com
+
+// All rights reserved.
+//
+
+// -- Currency Recipes
 ServerEvents.recipes(event => {
   
-    event.remove({ output: 'explorerscompass:explorerscompass' })
+  event.shapeless(
+  Item.of('modified:pokedollar', 9),
+  [
+    'modified:coinpile_pokedollar'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:mtoken', 9),
+  [
+    'modified:coinpile_mtoken'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:coinpile_pokedollar', 4),
+  [
+    'modified:coinblock_pokedollar'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:coinpile_mtoken', 4),
+  [
+    'modified:coinblock_mtoken'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:coinpile_pokedollar', 1),
+  [
+    '9x modified:pokedollar'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:coinpile_mtoken', 1),
+  [
+    '9x modified:mtoken'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:coinblock_pokedollar', 1),
+  [
+    '4x modified:coinpile_pokedollar'
+  ])
+});
+
+ServerEvents.recipes(event => {
+  
+  event.shapeless(
+  Item.of('modified:coinblock_mtoken', 1),
+  [
+    '4x modified:coinpile_mtoken'
+  ])
+});
+
+// -- Misc. Items
+ServerEvents.recipes(event => {
+
+    event.remove({output:'explorerscompass:explorerscompass'});
     event.shaped(
-    'explorerscompass:explorerscompass',
+      'explorerscompass:explorerscompass',
     [
-      'EGE',
-      'MRD',
-      'EFE'
+      'EME',
+      'SCD',
+      'EAE'
     ],
     {
       E: 'minecraft:ender_eye',
-      R: 'minecraft:recovery_compass',
-      D: 'minecraft:diamond_block',
-      F: 'alltheores:fluorite_block',
-      G: 'minecraft:gold_block',
-      M: 'minecraft:emerald_block'
+      C: 'minecraft:recovery_compass',
+      A: 'minecraft:ancient_debris',
+      D: 'deeperdarker:heart_of_the_deep',
+      M: 'cobblemon:master_ball',
+      S: 'minecraft:heart_of_the_sea'
     })
 });
 
+// -- Cobblemon Specific
 ServerEvents.recipes(event=> {
-  event.remove({id: "cobblemon:master_ball"})
+  event.remove({output:'cobblemon:master_ball'})
+});
+
+ServerEvents.recipes(event=> {
+
+  event.remove({id: 'tomtaru:mixer/hyper_potion_from_sitrus'})
 });
 
 ServerEvents.recipes(event => {
@@ -50,4 +141,62 @@ ServerEvents.recipes(event => {
     A: 'minecraft:golden_apple',
     S: 'supplementaries:sugar_cube'
   })
-})
+});
+
+ // Moon Event Items
+ServerEvents.recipes(event => {
+    const keystone = 'mega_showdown:keystone';
+
+    // -- Crafting Recipes for Moon Crescents
+    event.shapeless('modified:moon_crescent_blue', [
+        'modified:moon_fragment_blue', 
+        'modified:moon_fragment_blue', 
+        keystone                     
+    ]).id('kubejs:moon_crescent_blue_crafting'); 
+
+    event.shapeless('modified:moon_crescent_blood', [
+        'modified:moon_fragment_blood',
+        'modified:moon_fragment_blood',
+        keystone
+    ]).id('modified:moon_crescent_blood_crafting');
+
+    event.shapeless('modified:moon_crescent_harvest', [
+        'modified:moon_fragment_harvest',
+        'modified:moon_fragment_harvest',
+        keystone
+    ]).id('modified:moon_crescent_harvest_crafting');
+
+    // -- Crafting Recipes for SUPER Moon Crescents
+    event.shapeless('modified:super_moon_crescent_blue', [
+        'modified:moon_crescent_blue', 
+        'modified:moon_crescent_blue', 
+        keystone                     
+    ]).id('modified:super_moon_crescent_blue_crafting');
+
+    event.shapeless('modified:super_moon_crescent_blood', [
+        'kmodified:moon_crescent_blood',
+        'modified:moon_crescent_blood',
+        keystone
+    ]).id('modified:super_moon_crescent_blood_crafting');
+
+    event.shapeless('modified:super_moon_crescent_harvest', [
+        'modified:moon_crescent_harvest',
+        'modified:moon_crescent_harvest',
+        keystone
+    ]).id('modified:super_moon_crescent_harvest_crafting');
+});
+
+{
+console.log('$$$ MODIFIED RECIPES LOADED $$$')
+}
+
+//
+// Copyright (c) 2025 ImpavidEh - https://www.curseforge.com/members/impavideh/projects 
+// (@Jaker.zzz on Discord) - https://discord.gg/6R6Ch2mNgZ
+// 
+// 
+// This file is for use only in "MODIFIED COBBLEMON" projects.
+// https://ModifiedMC.com
+
+// All rights reserved.
+//
